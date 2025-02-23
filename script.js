3cb70c5b3f6111fc3c1f587d18279376056bdd05
Rohit Rautela <rohitrautela3231@gmail.com>
2025-02-23 18:05:08 +0530

    updated basic js and css

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-let var1=0;
-let var2=0;
-let operator=document.querySelectorAll(".operator");
-let operands=document.querySelectorAll(".operands");
-
+let var1="";
+let operands=document.querySelectorAll(".operand");
+let display=document.querySelector(".display");
 operands.forEach((select1)=>{
     select1.addEventListener("click",()=>{
-        select1.innerText=var1;
-        console.log(var1);
+        var1+=select1.innerText;
+        display.innerText=var1;
+        let x="";
+        let y="";
+        let l=var1.length;
+        let end=var1[l-1];
+        let indexsymbol=0;
+        if(end==="="){
+            for (let i = 0; i < l; i++) {
+                if (var1[i]==="+" || var1[i]==="-" || var1[i]==="×" || var1[i]==="÷") {
+                    indexsymbol=i;
+                    break;
+                }
+            }
+            for(let i=0; i < indexsymbol; i++){
+                x+=var1[i];
+            }
+            for(let i=indexsymbol+1; i < l-1; i++){
+                y+=var1[i];
+            }
+            x=+x;
+            y=+y;
+            console.log("x =  ",x)
+            console.log("y =  ",y)
+            if (var1[indexsymbol]==="+") {
+                display.innerText=x+y;
+            }
+            else if(var1[indexsymbol]==="-") {
+                display.innerText=x-y;
+            }
+            else if(var1[indexsymbol]==="×") {
+                display.innerText=x*y;
+            }
+            else if(var1[indexsymbol]==="÷") {
+                if(y===0){
+                    display.innerText="Math Error"
+                }
+                else{
+                    display.innerText=x/y;
+                }
+            }
+            var1="";
+        }
+        if(end==="C"){
+            var1="";
+            display.innerText="00.00";
+        }
+
     })
 })
